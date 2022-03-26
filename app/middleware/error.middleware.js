e7880caea7d36b064d2ef2ac82aaf5456143099b
Bishko86 <bishko86@yahoo.com>
2022-03-26 20:36:50 +0200

    #17 | create handler api and middleware

--- app/middleware/error.middleware.js
+++ app/middleware/error.middleware.js
@@ -1,11 +1,10 @@
 const ApiError = require("../exceptions/api.error");
 
 module.exports = (err, req, res, next) => {
-  console.log("ERROR:", err);
   if (err instanceof ApiError) {
     return res
       .status(err.status)
       .json({ message: err.message, errors: err.errors });
   }
-  return res.status(500).json({ message: "Unknown error", err });
+  return res.status(500).json({ message: "Unknown error" });
 };
